3d39bd9ba4b112a7c4084cb78f31589e39209bc7
Zibi Braniecki <gandalf@mozilla.com>
2014-12-20 17:48:53 -0800

    proper scrolling

--- js/chart.js
+++ js/chart.js
@@ -2,23 +2,25 @@
 
 function BatteryChart(data) {
   this.svg = null;
-  this.height = 250;
-  this.width = 450;
-  this.margin = {top: 20, right: 20, bottom: 30, left: 60};
+  this.height = 650;
+  this.width = 800;
+  this.margin = {top: 20, right: 20, bottom: 30, left: 100};
   this.data = data;
 
   this.x = d3.time.scale()
     .range([0, this.width]);
 
   this.y = d3.scale.linear()
-    .range([this.height, 0]);
+    .range([this.height, 0])
 
   this.xAxis = d3.svg.axis()
     .scale(this.x)
+    .ticks(3)
     .orient("bottom");
 
   this.yAxis = d3.svg.axis()
     .scale(this.y)
+    .ticks(2)
     .tickFormat(d3.format('%'))
     .orient("left");
 
@@ -34,30 +36,24 @@ function BatteryChart(data) {
   var min = d3.min(this.data.levelHistory, function(d) { return d[0]; });
   var max = d3.max(this.data.levelHistory, function(d) { return d[0]; });
 
-  this.slice = {
-    start: min + (2 * 60 * 60 * 1000),
-    end: min + (8 * 60 * 60 * 1000),
-  };
+  var now = new Date();
+  now.setMinutes(0);
+  now.setSeconds(0, 0);
+  now = now.getTime();
 
-  document.getElementById('b').addEventListener('click', function() {
-    this.slice = {
-      start: min + (4 * 60 * 60 * 1000),
-      end: min + (10 * 60 * 60 * 1000),
-    };
-    drawAxis.call(this);
-    drawArea.call(this, this.data.levelHistory);
-  }.bind(this));
+  this.slice = {
+    start: now - (14 * 60 * 60 * 1000),
+    end: now - (2 * 60 * 60 * 1000),
+  };
 }
 
 BatteryChart.prototype.draw = function() {
 
-  var height = this.height + (this.data.usage.length * 20);
+  var height = this.height + (this.data.usage.length * 80);
 
   var zoom = d3.behavior.drag()
     .on("drag", function(e, i) {
-      var dx = (d3.event.dx/500 * 6 * 60 * 60 * 1000);
-      this.slice.start += dx;
-      this.slice.end += dx;
+      updateChunk.call(this, d3.event.dx);
     drawAxis.call(this);
     drawArea.call(this, this.data.levelHistory);
     }.bind(this));
@@ -65,9 +61,9 @@ BatteryChart.prototype.draw = function() {
   this.svg = d3.select("body").append("svg")
     .attr("width", this.width + this.margin.left + this.margin.right)
     .attr("height", height + this.margin.top + this.margin.bottom)
+    .call(zoom)
     .append("g")
     .attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")")
-    .call(zoom);
 
   this.x.domain([this.slice.start, this.slice.end]);
   this.svg.append("g")
@@ -81,7 +77,33 @@ BatteryChart.prototype.draw = function() {
 
   drawLevel.call(this);
   //drawEstimation.call(this);
-  //drawUsageBars.call(this);
+  drawUsageBars.call(this);
+}
+
+function updateChunk(dx) {
+  var dx = (dx/450 * 12 * 60 * 60 * 1000);
+  var min = d3.min(this.data.levelHistory, function(d) { return d[0]; });
+  var max = d3.max(this.data.levelHistory, function(d) { return d[0]; });
+
+  if (dx > 0) {
+    if (min <= this.slice.start - dx) {
+      this.slice.start -= dx;
+      this.slice.end -= dx;
+    } else {
+      var dx = this.slice.start - min;
+      this.slice.start -= dx;
+      this.slice.end -= dx;
+    }
+  } else if (dx < 0) {
+    if (max > this.slice.end - dx) {
+      this.slice.start -= dx;
+      this.slice.end -= dx;
+    } else {
+      dx = max - this.slice.end;
+      this.slice.start += dx;
+      this.slice.end += dx;
+    }
+  }
 }
 
 function drawLevel() {
@@ -111,6 +133,7 @@ function getDataChunk(data) {
     return d[0] >= this.slice.start;
   }, this);
 
+
   var end = data.length - 1;
 
   var i = start;
@@ -120,7 +143,32 @@ function getDataChunk(data) {
       break;
     }
   }
-  return data.slice(start, end);
+
+  var prefix = [];
+  var postfix = [];
+  if (data[start][0] > this.slice.start) {
+    var dx0 = (data[start][0] - data[start - 1][0]); 
+    var dx1 = (data[start][0] - this.slice.start) / dx0;
+    var dy0 = data[start][1] - data[start - 1][1];
+    var dy1 = dy0 * dx1;
+    prefix.push([
+      this.slice.start,
+      data[start][1] - dy1
+    ]);
+  }
+
+  if (data[end - 1][0] < this.slice.end &&
+      end < data.length) {
+    var dx0 = data[end][0] - data[end - 1][0]; 
+    var dx1 = (this.slice.end - data[end - 1][0]) / dx0;
+    var dy0 = data[end][1] - data[end - 1][1];
+    var dy1 = dy0 * dx1;
+    postfix.push([
+      this.slice.end,
+      data[end - 1][1] + dy1
+    ]);
+  }
+  return prefix.concat(data.slice(start, end), postfix);
 }
 
 
@@ -144,18 +192,19 @@ function drawUsageBars() {
   this.data.usage.forEach(function(usage, i) {
     
     var x = 0;
-    var y = this.height + 20 + (i * 20);
+    var y = this.height + 30 + (i * 80);
     this.svg.append('rect')
       .attr('width', this.width)
-      .attr('height', 15)
+      .attr('height', 70)
       .attr('x', x)
       .attr('y', y)
       .attr('class', 'stackedbar');
 
     this.svg.append("text")
-      .attr("x", -50)
-      .attr("y", y + 8)
+      .attr("x", -95)
+      .attr("y", y + 36)
       .attr("dy", ".35em")
+      .attr('class', 'sb-text')
       .text(usage.label);
 
 
@@ -166,7 +215,7 @@ function drawUsageBars() {
       .attr('x', d => this.x(d[0]))
       .attr('y', y)
       .attr('width', d => this.x(d[1]) - this.x(d[0]))
-      .attr('height', 15)
+      .attr('height', 30)
       .attr('class', 'sb-' + usage.id);
 
   }, this);
